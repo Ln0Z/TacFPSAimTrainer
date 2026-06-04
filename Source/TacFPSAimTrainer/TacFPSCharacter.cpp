40448a52fc6245436637d1ffa4214e328d41cc04
Ahmed-Zayne El husseini <113213346+Ln0Z@users.noreply.github.com>
2026-06-04 19:53:14 +1000

    Moved weapon firing logic to weapnos blueprint, added enemy BP for peeking tutorial

--- Source/TacFPSAimTrainer/TacFPSCharacter.cpp
+++ Source/TacFPSAimTrainer/TacFPSCharacter.cpp
@@ -9,7 +9,8 @@ ATacFPSCharacter::ATacFPSCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 
-	PrimaryActorTick.bCanEverTick = true; bUseControllerRotationYaw = true;
+	PrimaryActorTick.bCanEverTick = true; 
+    bUseControllerRotationYaw = true;
     bUseControllerRotationPitch = false;
     bUseControllerRotationRoll = false;
 
